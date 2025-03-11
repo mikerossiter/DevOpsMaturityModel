3f940e39cd46995a328af4b50d7792cb37dfe895
Mike Rossiter <git.cusp521@silomails.com>
2025-03-11 19:59:11 +0000

    Add Trivy scanning of Docker image to pipeline for extra security

--- trivy-setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-export TRIVY_VERSION=$(wget -qO - "https://api.github.com/repos/aquasecurity/trivy/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
-echo "Using Trivy version: $TRIVY_VERSION"
-wget --no-verbose https://github.com/aquasecurity/trivy/releases/download/v${TRIVY_VERSION}/trivy_${TRIVY_VERSION}_Linux-64bit.tar.gz -O - | tar -zxvf -
